385e7652159af5b78dcc238584255ac1b973bfe5
faridlorestani <farid.lorestani@gmail.com>
2022-12-06 12:50:16 +0100

    Implement Controller for Catalog.Api

---
+++ src/Services/Catalog/Catalog.Api/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Repositories;
+
+namespace Catalog.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Catalog.Api", Version = "v1" });
+            });
+            services.AddScoped<ICatalogContext, CatalogContext>(); // this line means when asp.net see this
+                                                                   // automatically will create CatalogContext when it 
+                                                                   //sees ICatalogContext
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
