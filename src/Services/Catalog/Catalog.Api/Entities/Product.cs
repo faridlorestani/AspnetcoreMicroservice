f1c3f38ec99e544793f3af7e7c222684380e6652
faridlorestani <farid.lorestani@gmail.com>
2022-12-06 11:52:36 +0100

    implementing data and repository for Catalog-Api

---
+++ src/Services/Catalog/Catalog.Api/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Api.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("Name")] // you can give the column name here if you want to change the property name in the class
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string? Summary { get; set; }
+        public string? Description { get; set; }
+        public string? ImageFile { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
