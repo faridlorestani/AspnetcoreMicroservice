9836c19ccae04b2d2f6ffcc89082e3e84e16f670
faridlorestani <farid.lorestani@gmail.com>
2022-12-07 16:10:03 +0100

    - finish Catalog.Api - add docker file and docker-compose - create the image and container for Catalog.Api with required mongo container

--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<ICatalogContext, CatalogContext>(); // this line means when asp.net see this
                                                                // automatically will create CatalogContext when it 
                                                                //sees ICatalogContext
